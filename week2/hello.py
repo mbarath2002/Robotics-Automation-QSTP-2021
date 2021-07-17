47a98908eddd5f49e33c20f92eb65aac8d8ee845
mbarath2002 <mbarath2002@gmail.com>
2021-07-17 21:17:24 +0530

    Create hello.py

---
+++ week2/hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+import rospy
+from std_msgs.msg import String
+rospy.init_node('hello')
+pub=rospy.Publisher('hello',String)
+rate=rospy.Rate(2)
+
+c="Hello"
+
+while not rospy.is_shutdown()
+  pub.publish(c)
+  rate.sleep()
+  
