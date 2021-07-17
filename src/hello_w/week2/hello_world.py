9d4b4774be850f195bfae6ec177f0a772b02c3fa
mbarath2002 <mbarath2002@gmail.com>
2021-07-17 23:46:10 +0530

    Update hello_world.py

--- src/hello_w/week2/hello_world.py
+++ src/hello_w/week2/hello_world.py
@@ -3,11 +3,13 @@ import rospy
 from std_msgs.msg import String
 rospy.init_node('hello_world')
 
-def callback(msg):
+def callback1(msg):
+  print(msg.data)
+def callback2(msg):
   print(msg.data)
   
 if __name__=='__main__':
-  sub1=rospy.Subscriber('hello',String,callback)
-  sub2=rospy.Subscriber('world',String,callback)
+  sub1=rospy.Subscriber('hello',String,callback1)
+  sub2=rospy.Subscriber('world',String,callback2)
   rospy.spin()
   
