8ed67ecab08fe18f6d1f459e2729e3e12da37e23
mbarath2002 <mbarath2002@gmail.com>
2021-07-17 21:20:42 +0530

    Create world.py

---
+++ week2/world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.7
+import rospy
+from std_msgs.msg import String
+rospy.init_node('receiver')
+pub=rospy.Publisher('world',String)
+rate=rospy.Rate(2)
+
+c="World"
+
+while not rospy.is_shutdown()
+  pub.publish(c)
+  rate.sleep()
+  
