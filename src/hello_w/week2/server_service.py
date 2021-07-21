6ae7c0151eab0d5a828a5cb80617ecf1ccbd0fac
mbarath2002 <mbarath2002@gmail.com>
2021-07-21 14:09:20 +0530

    Update server_service.py

--- src/hello_w/week2/server_service.py
+++ src/hello_w/week2/server_service.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python2.7
+rospy.init_node('server_node')
+import rospy
+import math
+from hello_w.srv import state
 
+def callback(service):
+  
+  
+rospy.Server('server',state,callback)
+
+if __name__=="__main__":
+  
