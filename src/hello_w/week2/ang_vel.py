52d9dd3b93f39b98f3ba0de879e807c8a4c9c6b2
mbarath2002 <mbarath2002@gmail.com>
2021-07-21 20:06:25 +0530

    Update ang_vel.py

--- src/hello_w/week2/ang_vel.py
+++ src/hello_w/week2/ang_vel.py
@@ -4,14 +4,10 @@ rospy.init_node('ang_vel')
 import rospy
 from std_msgs.msg import int32
 from hello_w.srv import server,serverResponse
-def callback(msg):
   w=0.1/(msg.data)
-
 def ang_vel(request):
-  return serverResponse(omega)
+  return omegaResponse(0.1/(request.r))
 
-sub=rospy.Subscriber('radius',int32,callback)
-
-service=rospy.Service('compute_ang_vel',omega,callback)
+service=rospy.Service('compute_ang_vel',omega,ang_vel)
 
 rospy.spin()
