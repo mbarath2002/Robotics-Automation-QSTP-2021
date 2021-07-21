86b13006a601ea6a8680ec536418bfdfcbb048e3
mbarath2002 <mbarath2002@gmail.com>
2021-07-21 15:42:07 +0530

    Update ang_vel.py

--- src/hello_w/week2/ang_vel.py
+++ src/hello_w/week2/ang_vel.py
@@ -1,1 +1,12 @@
 
+#!/usr/bin/env python2.7
+rospy.init_node('ang_vel')
+import rospy
+from std_msgs.msg import int32
+
+def callback(msg):
+  omega=0.1/(msg.data)
+  
+sub=rospy.Subscriber('radius',int32,callback)
+
+service=rospy.Service('compute_ang_vel',ang,omega)
