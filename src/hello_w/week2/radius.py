952cd70aa289bbb1ed0a9491e7269d4fa74cadc4
mbarath2002 <mbarath2002@gmail.com>
2021-07-21 15:32:42 +0530

    Update radius.py

--- src/hello_w/week2/radius.py
+++ src/hello_w/week2/radius.py
@@ -1,1 +1,14 @@
 
+#!/usr/bin/env python2.7
+import rospy
+from std_msgs.msg import int32
+rospy.init_node('publisher')
+rate=rospy.Rate(2)
+pub=rospy.Publisher('radius',int32)
+
+r=input()
+
+while not rospy.is_shutdown():
+  pub.publish(r)
+  rate.sleep()
+
