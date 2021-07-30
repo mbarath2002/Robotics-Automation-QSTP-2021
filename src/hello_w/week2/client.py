a20496d42dbf91a15c47d23de34e1ac7cc6fee6c
mbarath2002 <mbarath2002@gmail.com>
2021-07-30 13:50:50 +0530

    Update client.py

--- src/hello_w/week2/client.py
+++ src/hello_w/week2/client.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs import float32
+from state.srv import stateResponse, state
+import sys
 
+rospy.init_node('client')
+rospy.wait_for_service('position')
+
+finder=rospy.ServiceProxy('position',state)
+
+position=finder(x)
