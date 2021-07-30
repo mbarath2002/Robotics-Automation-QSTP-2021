098a95ac5c09ece08076c9d7eb5da7ba77984ff1
mbarath2002 <mbarath2002@gmail.com>
2021-07-30 18:22:30 +0530

    Update mover.py

--- src/hello_w/week2/mover.py
+++ src/hello_w/week2/mover.py
@@ -4,7 +4,7 @@ import rospy
 from std_msgs.msg import Float32
 from hello_w.srv import omega
 from geometry_msgs.msg import Twist
-
+r=1
 v=0.1
 rospy.init_node('mover')
 pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
