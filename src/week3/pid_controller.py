db4b90f3a3368237485723aebd74624485556593
mbarath2002 <mbarath2002@gmail.com>
2021-07-31 03:22:56 +0530

    Update pid_controller.py

--- src/week3/pid_controller.py
+++ src/week3/pid_controller.py
@@ -3,20 +3,24 @@ import rospy
 from nav_msgs.msg import Path
 from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion
 from std_msgs.msg import Header,float32
+from tf.transformations import euler_from_quaternion as eu
 import sys 
 
-def callback1(msg):
-  
-def callback2(msg):
-  
-def callback3(msg):
-  
 
-if __name__ == "__main__":
-  rospy.init_node('controller')
-  rate=rospy.Rate(5)
+def callback1(msg):
+  goal=msg.poses(end).pose.position.y
+  start=0
+  sub=rospy.Subscriber('odom',Odometry)
+#def callback2(msg):
+ 
+#def callback3(msg):
+def odom(msg):
   
-  sub1=rospy.Subscriber('path1',Path,callback1)
-  sub2=rospy.Subscriber('path2',Path,callback2)
-  sub3=rospy.Subscriber('path3',Path,callback3)
+  
+rospy.init_node('controller')
+rate=rospy.Rate(5)
+  
+sub1=rospy.Subscriber('path1',Path,callback1)
+#sub2=rospy.Subscriber('path2',Path,callback2)
+#sub3=rospy.Subscriber('path3',Path,callback3)
   
