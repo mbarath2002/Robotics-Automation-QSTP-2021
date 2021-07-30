ce159b5bab4005587bdacc614ba36aec13fe8a0b
mbarath2002 <mbarath2002@gmail.com>
2021-07-30 18:19:45 +0530

    Update mover.py

--- src/hello_w/week2/mover.py
+++ src/hello_w/week2/mover.py
@@ -5,25 +5,26 @@ from std_msgs.msg import Float32
 from hello_w.srv import omega
 from geometry_msgs.msg import Twist
 
+v=0.1
 rospy.init_node('mover')
 pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
+rate=rospy.Rate(2)
 
 def callback(msg):
-  rate=rospy.Rate(2)
-  rospy.wait_for_service('compute_ang_vel')
-  ang_vel_calculator=rospy.ServiceProxy('compute_ang_vel',omega)
+  global r
   r=msg.data
-  w=ang_vel_calculator(r)
-  v=0.1
-  move=Twist()
-  move.linear.x=v
-  move.angular.z=w
-  while not rospy.is_shutdown():
-    pub.publish(move)
-    rate.sleep()
-
+  
 sub=rospy.Subscriber('radius',Float32,callback)
+rospy.wait_for_service('compute_ang_vel')
+ang_vel_calculator=rospy.ServiceProxy('compute_ang_vel',omega)
+w=ang_vel_calculator(r)
 
+move=Twist()
+move.linear.x=v
+move.angular.z=w
+while not rospy.is_shutdown():
+  pub.publish(move)
+  rate.sleep()
 
 
 
