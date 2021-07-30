df90ca06ccc2400b6f0e91c8c14ad08a4d6b0e0a
mbarath2002 <mbarath2002@gmail.com>
2021-07-30 17:50:50 +0530

    Update ang_vel.py

--- src/hello_w/week2/ang_vel.py
+++ src/hello_w/week2/ang_vel.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import Float32
 from hello_w.srv import omega,omegaResponse
 def ang_vel(request):
-  return omegaResponse(0.1/(request.r))
+  return 0.1/(request.radius)
 
 rospy.init_node('ang_vel')
 service=rospy.Service('compute_ang_vel',omega,ang_vel)
